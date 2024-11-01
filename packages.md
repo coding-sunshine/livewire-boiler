0b8e4744be2d8542c46f5b21c1e8c6ad5b663a79
Hardik Shah <sunshine.mahant@gmail.com>
2024-11-01 19:21:45 +0530

    added spatie/Laravel-error-solutions and spatie/laravel-schedule-monitor
    added Laravel-error-solutions and openai php for AI based suggestions on local
    added spatie/laravel-schedule-monitor

---
+++ packages.md
@@ -0,0 +1,3 @@
+## Added Packages
+- [**`spatie/laravel-error-solutions`**](https://spatie.be/docs/laravel-error-solutions/v1/introduction) - Display solutions on the Laravel error page.
+- [**`spatie/laravel-schedule-monitor`**](https://github.com/spatie/laravel-schedule-monitor) - Monitor scheduled tasks in Laravel.
--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\Scheduling\Schedule;
+use Spatie\ScheduleMonitor\Models\MonitoredScheduledTaskLogItem;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+app(Schedule::class)->command('model:prune', ['--model' => MonitoredScheduledTaskLogItem::class])->daily();

ca39b64fa4d79d7dbb6395c1b3505e5e0e551617
Hardik Shah <sunshine.mahant@gmail.com>
2024-11-01 19:56:42 +0530

    make sure to avoid n+1 query issues
    https://msamgan.com/how-to-detect-and-fix-n1-queries-in-laravel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureModels();
+    }
+
+    /**
+     * Configure the models.
+     */
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict(! $this->app->isProduction());
     }
 }

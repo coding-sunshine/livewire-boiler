19826ea4870fb82c917ff60c28cff1c38b781c0e
Hardik Shah <hshah@goodtogreatschools.org.au>
2024-12-08 13:16:45 +0530

    Appwide Settings for future

---
+++ config/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Str;
+
+return [
+
+    /*
+     * Auth status enabled or disabled
+     */
+    'auth' => env('AUTH_ENABLED', false),
+];
--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+if(config('settings.auth')) {
+    Route::view('dashboard', 'dashboard')
+        ->middleware(['auth', 'verified'])
+        ->name('dashboard');
 
-require __DIR__ . '/auth.php';
+    Route::view('profile', 'profile')
+        ->middleware(['auth'])
+        ->name('profile');
+
+    require __DIR__ . '/auth.php';
+}
+
+
+
+
+
